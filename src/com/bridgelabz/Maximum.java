312c39b89d449d82f17caf8552d2d2009f3d9d26
Ashwini Rathod <rashwini510@gmail.com>
2022-06-17 20:57:23 +0530

    Added: Check the maximum among three int

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,7 +1,33 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Maximum {
     public static void main(String[] args) {
-        System.out.println("Welcome to maximum generics program");
+        //creating object
+        Maximum object = new Maximum();
+        System.out.println("Maximum among three int is : " + object.compareTo());
+    }
+
+    // compareTo method for checking max int
+    public Integer compareTo() {
+        Scanner scanner = new Scanner(System.in);
+        // User Input
+        System.out.println("Enter three number");
+        int p = scanner.nextInt();
+        int q = scanner.nextInt();
+        int r = scanner.nextInt();
+        int maximum = p;
+
+        if (p > q && p > r) {
+            maximum = p;
+        } else if (q > p && q > r) {
+            maximum = q;
+
+        } else {
+            maximum = r;
+        }
+        return maximum;
     }
 }
+
