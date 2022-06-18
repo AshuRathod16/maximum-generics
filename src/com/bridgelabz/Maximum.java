b02477df42a81ca14c3c57c3012c9df21773ed01
Ashwini Rathod <rashwini510@gmail.com>
2022-06-18 19:48:45 +0530

    Added: Add more than three parameter and find maximum using sorting

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,31 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+
 //Generic Class
-public class Maximum<T extends Comparable<T>> {
-    T a, b, c;
+public class Maximum {
+    public static  <T> T maximum(T...a) {
 
-    public Maximum(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
+        Arrays.sort(a);
+        return a[a.length - 1];
     }
 
     // main method
     public static void main(String[] args) {
-        System.out.printf("Maximum of %d, %d and %d is %d \n ", 10, 20, 30, maximum(10, 20, 30));
-        System.out.printf("Maximum of %f, %f and %f is %f \n ", 1.1, 2.2, 3.3, maximum(1.1, 2.2, 3.3));
-        System.out.printf("Maximum of %s, %s and %s is %s \n ", "Apple", "Orange", "Banana", maximum("Apple", "Orange", "Banana"));
+        System.out.println("Welcome to maximum generic program");
+        System.out.println("Maximum among three integer is:" + maximum(10,35,-25,-40,48));
+        System.out.println("Maximum among three float is:" + maximum(3.5f,4.7f,6.8f,2.9f,3.3f));
+        System.out.println("Maximum among three string is:" + maximum("Apple", "Orange", "Banana", "Strawberry", "Peach"));
     }
 }
 
